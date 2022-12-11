2cd2479f798c1249ffbcdee7075c3a907e17410f
Sinan <haxsmith77@gmail.com>
2022-12-12 04:18:09 +1100

    Updated NavBar

---
+++ bitcoin-frontend/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar theme="dark" flat>
+    <v-toolbar-title>Bitcoin</v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat>
+        <span>Sign out</span>
+      </v-btn>
+      <v-btn flat>
+        <span>Sign out</span>
+      </v-btn>
+      <v-btn flat>
+        <span>Sign out</span>
+      </v-btn>
+      <v-btn flat>
+        <span>Sign out</span>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
