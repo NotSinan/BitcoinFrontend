65d769890883eb445505174107c017a99a2f0dd3
Sinan Koparan <haxsmith77@gmail.com>
2022-12-12 00:22:03 +1100

    Added new components
    Added new components

--- bitcoin-frontend/src/App.vue
+++ bitcoin-frontend/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <v-parallax
-    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-  ></v-parallax>
-  <v-parallax
-    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-  ></v-parallax>
-  <v-parallax
-    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-  ></v-parallax>
-  <v-parallax
-    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-  ></v-parallax>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :prepend-icon="
+          theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+        "
+        @click="onClick"
+        >Toggle Theme</v-btn
+      >
+    </v-app-bar>
+
+    <Parallax />
+    <Footer />
+  </v-app>
 </template>
 
-<script>
-export default {
-  name: "App",
+<script setup>
+import Parallax from "./components/Parallax.vue";
+import Footer from "./components/Footer.vue";
+import { ref } from "vue";
 
-  components: {},
+const theme = ref("light");
 
-  data: () => ({
-    //
-  }),
-};
+function onClick() {
+  theme.value = theme.value === "light" ? "dark" : "light";
+}
 </script>
---
+++ bitcoin-frontend/src/components/Parallax.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    <div
+      class="d-flex flex-column fill-height justify-center align-center text-white"
+    >
+      <h1 class="text-h4 font-weight-thin mb-4">Bitcoin</h1>
+      <h4 class="subheading">
+        Bitcoin is an innovative payment network and a new kind of money!
+      </h4>
+    </div>
+  </v-parallax>
+</template>
