2ff592c9f61bfc083f28872515a6e6ce11138de1
Sinan <haxsmith77@gmail.com>
2022-12-21 19:21:48 +1100

    NAv changes

--- bitcoin-frontend/src/components/NavBar.vue
+++ bitcoin-frontend/src/components/NavBar.vue
@@ -1,20 +1,26 @@
 <template>
-  <v-toolbar theme="dark" flat>
-    <v-toolbar-title>Bitcoin</v-toolbar-title>
-    <v-toolbar-items>
-      <DropDown />
-      <v-btn flat>
-        <span>Sign out</span>
-      </v-btn>
-      <v-btn flat>
-        <span>Sign out</span>
-      </v-btn>
-      <v-btn flat>
-        <span>Sign out</span>
-      </v-btn>
-    </v-toolbar-items>
-    <v-spacer></v-spacer>
-  </v-toolbar>
+  <div>
+    <v-toolbar
+      dark
+      flat
+      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-toolbar-title>Bitcoin</v-toolbar-title>
+      <v-toolbar-items>
+        <DropDown />
+        <v-btn flat>
+          <span>Sign out</span>
+        </v-btn>
+        <v-btn flat>
+          <span>Sign out</span>
+        </v-btn>
+        <v-btn flat>
+          <span>Sign out</span>
+        </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
